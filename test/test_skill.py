9e8a002312661784cd7fef0dd6de78e0b72f7a39
Daniel McKnight <daniel@neon.ai>
2024-02-01 13:28:52 -0800

    Update tests to resolve failures

--- test/test_skill.py
+++ test/test_skill.py
@@ -17,53 +17,16 @@
 # US Patents 2008-2021: US7424516, US20140161250, US20140177813, US8638908, US8068604, US8553852, US10530923, US10530924
 # China Patent: CN102017585  -  Europe Patent: EU2156652  -  Patents Pending
 
-import shutil
-import unittest
-from threading import Event
-from time import sleep
-
 import pytest
 
-from os import mkdir
-from os.path import dirname, join, exists
+from threading import Event
+from time import sleep
 from mock import Mock
-from mycroft_bus_client import Message
-from ovos_utils.messagebus import FakeBus
+from ovos_bus_client.message import Message
+from neon_minerva.tests.skill_unit_test_base import SkillTestCase
 
 
-class TestSkill(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls) -> None:
-        from mycroft.skills.skill_loader import SkillLoader
-
-        cls.bus = FakeBus()
-        cls.bus.run_in_thread()
-        skill_loader = SkillLoader(cls.bus, dirname(dirname(__file__)))
-        skill_loader.load()
-        cls.skill = skill_loader.instance
-        cls.test_fs = join(dirname(__file__), "skill_fs")
-        if not exists(cls.test_fs):
-            mkdir(cls.test_fs)
-        cls.skill.settings_write_path = cls.test_fs
-        cls.skill.file_system.path = cls.test_fs
-
-        # cls.skill._init_settings()
-        # cls.skill.initialize()
-        # Override speak and speak_dialog to test passed arguments
-        cls.skill.speak = Mock()
-        cls.skill.speak_dialog = Mock()
-        cls.skill.schedule_event = Mock()
-
-    @classmethod
-    def tearDownClass(cls) -> None:
-        shutil.rmtree(cls.test_fs)
-
-    def tearDown(self) -> None:
-        self.skill.speak.reset_mock()
-        self.skill.speak_dialog.reset_mock()
-        self.skill.schedule_event.reset_mock()
-
+class TestSkillMethods(SkillTestCase):
     def test_00_skill_init(self):
         # Test any parameters expected to be set in init or initialize methods
         from neon_utils.skills import NeonSkill
